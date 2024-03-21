0f8eea0d2c6cdaf727e979068616c5e8064301df
LCarminatti <lucas.csantos@sptech.school>
2024-03-21 08:58:10 -0300

    Código atualizado arduino

---
+++ Código Arduino/C_digo/C_digo.ino
@@ -0,0 +1,27 @@
+//Sensor de temperatura usando o LM35
+ 
+const int LM35 = A0; // Define o pino que lera a saída do LM35
+float temperatura; // Variável que armazenará a temperatura medida
+float temperaturaMAX = 80;
+float temperaturaMIN = 40;
+
+ 
+//Função que será executada uma vez quando ligar ou resetar o Arduino
+void setup() {
+Serial.begin(9600); // inicializa a comunicação serial
+}
+ 
+//Função que será executada continuamente
+void loop() {
+temperatura = (float(analogRead(LM35))*5/(1023))/0.01; //Cálculo da tensão e o limite de trabalho do sensor; 11bits
+Serial.print("TemperaturaMax:");
+Serial.print(temperaturaMAX);
+Serial.print(',');
+Serial.print("TemperaturaMin:");
+Serial.print(temperaturaMIN);
+Serial.print(',');
+Serial.print("Temperatura:");
+Serial.println(temperatura + 40);
+delay(2000);
+}
+
